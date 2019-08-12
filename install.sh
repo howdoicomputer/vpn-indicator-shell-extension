009bf4783317e251eb20132fe6f8f99911ec9b32
amp5208 <arnoldpelka@gmail.com>
2019-08-11 17:13:39 -0700

    issue #3 resolved

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 dir=~/.local/share/gnome-shell/extensions/vpn-indicator@howdoicomputer.fastmail.com
 
-rm -rf $dir
+if [ -d $dir ]; then
+  rm -rf $dir
+else
+  echo "brand-new install..."
+fi
+mkdir -p $dir
 cp -R . $dir
+
+echo "installed:"
+ls -al $dir
