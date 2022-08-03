dc2a033df6e7c077d47df57411fcf47151af49cd
bpavle <32293848+bpavle@users.noreply.github.com>
2022-08-03 22:14:00 +0200

    Update install.sh
    Make sure to run script from project directory since there is a reference to it as a current direcotiry (.) 
    And by the way, thanks for the extension:)

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -e
+#Make sure to run script from project directory since there is a reference to it as a current direcotiry (.)
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+cd "$DIR"
 
 dir=~/.local/share/gnome-shell/extensions/vpn-indicator@howdoicomputer.fastmail.com
 
